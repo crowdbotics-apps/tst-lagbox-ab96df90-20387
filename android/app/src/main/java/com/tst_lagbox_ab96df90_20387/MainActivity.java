a06caa9903ae848f12ea4aa0b9c3dcbe84ed5b3d
Crowdbotics <team@crowdbotics.com>
2021-03-10 22:16:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tst_lagbox_ab96df90_20387/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tst_lagbox_ab96df90_20387;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "tst_lagbox_ab96df90_20387";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://tst-lagbox-ab96df90-20387.botics.co",
+  defaultTimeout: 5000
+};
